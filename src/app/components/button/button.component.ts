c0baa73e2c6e7a7b2dd502232aaf0003b08bdd81
apocalypse-suite <mitchenko.catherine@gmail.com>
2020-11-11 20:46:59 +0200

    scroll to bottom fixed

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {AfterViewChecked, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent implements OnInit, AfterViewChecked{
   @Input('text') text: string;
   @Input('route') route: string;
-  scrollToBottom(): void{
-    window.scrollTo(0, document.body.scrollHeight);
-  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
+  scrollToBottom(): void{
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
 }
